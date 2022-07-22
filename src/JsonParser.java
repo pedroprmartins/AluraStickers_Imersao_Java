8000435fff9f6377673ea78797e6e4b78341f381
pedro <pauloregistecnico@gmail.com>
2022-07-22 13:45:47 -0300

    Dasafio da aula 01 - ok

--- src/JsonParser.java
+++ src/JsonParser.java
@@ -13,16 +13,13 @@ public class JsonParser {
     public List<Map<String, String>> parse(String json) {
         Matcher matcher = REGEX_ITEMS.matcher(json);
         if (!matcher.find()) {
-
             throw new IllegalArgumentException("Não encontrou items.");
         }
 
         String[] items = matcher.group(1).split("\\},\\{");
-
         List<Map<String, String>> dados = new ArrayList<>();
 
         for (String item : items) {
-
             Map<String, String> atributosItem = new HashMap<>();
 
             Matcher matcherAtributosJson = REGEX_ATRIBUTOS_JSON.matcher(item);
@@ -31,11 +28,8 @@ public class JsonParser {
                 String valor = matcherAtributosJson.group(2);
                 atributosItem.put(atributo, valor);
             }
-
             dados.add(atributosItem);
         }
-
         return dados;
     } 
-
 }
